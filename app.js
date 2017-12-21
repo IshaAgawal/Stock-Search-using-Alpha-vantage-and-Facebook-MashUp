e6d0dbbe1d70664683bd7f2d48d5360af0f20dd3
Isha Agarwal <15099221+IshaAgawal@users.noreply.github.com>
2017-12-21 12:32:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require('express')
  
 const app = express()
-
-
 var request = require('request');
 var bodyParser = require('body-parser');
-
-
 var parseString = require('xml2js').parseString;
-
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
 app.use(bodyParser.json());
-
 app.get('/symbolData', function (req, res) {  
-
- 
 console.log(req.query.symbol);
 
 var symbolURL = ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=").concat(req.query.symbol)
@@ -56,10 +48,9 @@ parseString(response.body, function (err, result) {
 
 app.get('/indicatorData', function(req, res) {
 
-	var indicatorURL = ("https://www.alphavantage.co/query?function=").concat(req.query.indicator).concat("&symbol=").concat(req.query.symbol)
+var indicatorURL = ("https://www.alphavantage.co/query?function=").concat(req.query.indicator).concat("&symbol=").concat(req.query.symbol)
 						.concat("&interval=daily&time_period=10&series_type=close&apikey=A2YO93CBH2X5ELC2");
-
-	console.log(indicatorURL);
+console.log(indicatorURL);
 
 request(indicatorURL, function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -90,11 +81,6 @@ request(suggestionsURL, function (error, response, body) {
 })
 
 })
-
-
-
-
-
 app.listen(8081, function () {
   console.log('Example app listening on port 8081!')
 })
